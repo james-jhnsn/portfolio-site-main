7cb5cb05d72ab3ca8be15d12bf6b27cb3571988e
James Johnson <jamesjhnsn1995@gmail.com>
2023-01-22 17:22:06 -0800

    cleaned up file structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,10 @@
 import React from 'react'
 import aboutImage from '../assets/about-image.png'
 
-import { BiTachometer } from 'react-icons/bi'
-
 const About = () => {
 
 
     return (
-        // <div name='about' className='w-full bg-gradient-to-b from-gray-800 to-black text-white'>
-        //     <div className='max-w-screen-lg p-4 mx-auto flex-col justify-center w-full h-full'>
-        //         <div className='pb-8'>
-        //             <p className=' text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
-        //         </div>
-        //         <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nemo saepe fuga inventore in quibusdam porro recusandae distinctio iste. Expedita repellendus totam eos consequatur voluptatem. Voluptatum, ratione repellat, illo quidem tempore quas recusandae non dolore minima odit nisi. Animi exercitationem inventore officia. Vel natus reiciendis laudantium animi nulla quidem pariatur.</p>
-
-        //         <br />
-        //         <p className='text-xl'>
-        //             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos magnam quo aliquid et ipsa. Quis adipisci libero cum pariatur sunt!
-        //         </p>
-        //     </div>
-        // </div>
-
 
           <div name='ABOUT' className="grid grid-cols-1 md:grid-cols-2 items-center gap-8 py-16 px-32 h-screen">
              <img src={aboutImage} alt="" data-aos="fade-up" data-aos-delay="400" />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,26 +27,6 @@ const NavBar = () => {
     ]
 
     return (
-        // <div className='flex justify-end items-center w-full h-20 text-white fixed bg-black px-4'>
-        //     <ul className='absolute inset-x-0 top-20 flex  flex-col items-center gap-8 bg-slate-800 py-8 opacity-0 transition duration-300 peer-checked:translate-x-0 peer-checked:opacity-100 md:relative md:top-0 md:translate-x-0 md:flex-row md:bg-transparent md:py-0 md:opacity-100'>
-        //         {/* destructuring the array object */}
-        //         {links.map(({id, link}) =>(
-        //             <li key={id} className='text-white hover:border-b hover:border-white hover:pb-2 transition-all duration-300 px-2'>{link}</li>
-        //         ))}
-        //     </ul>
-        //     <div onClick={() => setNav(!nav)} className='cursor-pointer mr-4 z-10 text-white md:hidden'>
-        //         {/* the quetsion mark is an inline conditonal statement in javaScript */}
-        //         {nav ? <FaTimes size={30}/> : <FaBars size={30}/> }
-        //     </div>
-        //     {nav && (
-        //         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-white'>
-        //         {links.map(({id, link}) =>(
-        //             <li key={id} className='px-4 cursor-pointer capitalize my-6 text-4xl'>{link}</li>
-        //         ))}
-        //     </ul>
-        //     ) }
-
-        // </div>
 
         <div className="flex items-center justify-between bg-slate-800 px-32 fixed w-full z-10">
             <a >
